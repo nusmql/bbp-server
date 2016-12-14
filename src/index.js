9af05751c969b8f2f86064932a36e1e7ce0aa279
U-SG\Jack.Meng <Jack.Meng@SG217.sg.bwoil.com>
2016-12-14 18:33:05 +0800

    add example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import TableExamplePagination from './example/TableExamplePagination'
 
-ReactDOM.render((<h1>Hello World</h1>), document.getElementById('app'))
+
+ReactDOM.render(<TableExamplePagination />, document.getElementById('app'))
